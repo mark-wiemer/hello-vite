603d7881cdeca8686052b92dba684278b991fd2f
Mark Wiemer <markwiemer@outlook.com>
2023-09-07 20:44:48 -0700

    Use Vitest + React Testing Library

--- README.md
+++ README.md
@@ -3,9 +3,8 @@
 A batteries-included template project for Vite, React, and TypeScript
 
 -   Created using `npm create vite@4.4.1 hello-vite-three-fiber -- --template react-ts`
--   Includes [ESLint](https://eslint.org/), [Prettier](https://prettier.io), [Sort Package.json](https://github.com/keithamus/sort-package-json), [Husky](https://typicode.github.io/husky/), [lint-staged](https://github.com/okonet/lint-staged), and a `validate` script
+-   Includes [Vitest](https://vitest.dev), [React Testing Library](https://testing-library.com/docs/react-testing-library/intro), [ESLint](https://eslint.org/), [Prettier](https://prettier.io), [Sort Package.json](https://github.com/keithamus/sort-package-json), [Husky](https://typicode.github.io/husky/), [lint-staged](https://github.com/okonet/lint-staged), and a `validate` script.
 
 ## TODO
 
--   Unit test framework
 -   Integration test framework
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useCounter } from './hooks/useCounter';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const { count, increment } = useCounter();
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
+        <div className="App">
+            <header className="App-header">
+                <p>Hello Vite + React!</p>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    <button type="button" onClick={increment}>
+                        count is: {count}
+                    </button>
                 </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+                <p>
+                    Edit <code>App.tsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
     );
 }
 
