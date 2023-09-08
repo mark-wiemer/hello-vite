7a88a45c6dee05846f6cd59764c3c0d021f9f40d
Mark Wiemer <markwiemer@outlook.com>
2023-09-07 21:45:18 -0700

    Remove CSS-based unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,11 +12,4 @@ describe('Simple working test', () => {
         userEvent.click(screen.getByRole('button'));
         expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument();
     });
-
-    it('uses flexbox in app header', async () => {
-        render(<App />);
-        const element = screen.getByRole('banner');
-        expect(element.className).toEqual('App-header');
-        expect(getComputedStyle(element).display).toEqual('flex');
-    });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: './src/test/setup.ts',
-        // you might want to disable it, if you don't have tests that rely on CSS
-        // since parsing CSS is slow
-        css: true,
         include: ['**/*.test.?(c|m)[jt]s?(x)'],
     },
 });
