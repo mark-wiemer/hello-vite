a278a8bcd71eb5e782fea43a34d6ee3ffb6de6df
Mark Wiemer <markwiemer@outlook.com>
2023-09-07 20:07:07 -0700

    Fix lint TS files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-var x;
-var y;
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
