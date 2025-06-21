fed5811da2ba24504ac5c011447dd16da976b546
Mark Wiemer <markwiemer@outlook.com>
2025-06-21 07:29:11 -0700

    Revert "Add three.js, break unit tests"
    This reverts commit b2341fa40bdfa28f8d246d06310a11ddd38816a7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,43 @@
-import { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { useCounter } from './hooks/useCounter';
+import './App.css';
 
-function Box(props: { position: [number, number, number] }) {
-    // This reference gives us direct access to the THREE.Mesh object
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const ref = useRef<any>();
-    // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false);
-    const [clicked, click] = useState(false);
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((_, delta) => (ref.current.rotation.x += delta));
-    // Return the view, these are regular Threejs elements expressed in JSX
+function App() {
+    const { count, increment } = useCounter();
+
     return (
-        <mesh
-            {...props}
-            ref={ref}
-            scale={clicked ? 1.5 : 1}
-            onClick={(event) => (event.stopPropagation(), click(!clicked))}
-            onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-            onPointerOut={() => hover(false)}
-        >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
+        <div className="App">
+            <header className="App-header">
+                <p>Hello Vite + React!</p>
+                <p>
+                    <button type="button" onClick={increment}>
+                        count is: {count}
+                    </button>
+                </p>
+                <p>
+                    Edit <code>App.tsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
     );
 }
 
-export default function App() {
-    return (
-        <>
-            <h1>{'Hello Vite + React!'}</h1>
-            <Canvas>
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-                <pointLight position={[-10, -10, -10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
-                <OrbitControls />
-            </Canvas>
-        </>
-    );
-}
+export default App;
