6fba818e525bc7115b964345797dc150c87502fd
Mark Wiemer <markwiemer@outlook.com>
2025-06-22 09:30:45 -0700

    Update all remaining dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,45 @@ function App() {
     const { count, increment } = useCounter();
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>Hello Vite + React + TypeScript!</p>
-                <p>
-                    <button type="button" onClick={increment}>
-                        Count is: {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://vite.dev"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite docs
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://react.dev"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React docs
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://typescriptlang.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        TypeScript docs
-                    </a>
-                </p>
-            </header>
-        </div>
+        <>
+            <h1>Hello Vite + React + TypeScript!</h1>
+            <p>
+                <button type="button" onClick={increment}>
+                    Count is: {count}
+                </button>
+            </p>
+            <p>
+                Edit <code>App.tsx</code> and save to test HMR updates.
+            </p>
+            <p>
+                <a
+                    className="App-link"
+                    href="https://vite.dev"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Vite docs
+                </a>
+                {' | '}
+                <a
+                    className="App-link"
+                    href="https://react.dev"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    React docs
+                </a>
+                {' | '}
+                <a
+                    className="App-link"
+                    href="https://typescriptlang.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    TypeScript docs
+                </a>
+            </p>
+        </>
     );
 }
 
