8fbe1d99732a4642847f4c9a965a26e6972645fb
Mark Wiemer <markwiemer@outlook.com>
2025-06-22 10:35:23 -0700

    Remove retries prop from Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    retries: 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
