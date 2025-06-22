225aca55f0c52b84d1f8f1c0d884103f1ecd5bbd
Mark Wiemer <7833360+mark-wiemer@users.noreply.github.com>
2025-06-22 10:49:18 -0700

    Move reportUnusedDisableDirectives to config (#6)

--- eslint.config.js
+++ eslint.config.js
@@ -8,5 +8,8 @@ export default tseslint.config(
     tseslint.configs.recommended,
     {
         ignores: ['dist'],
+        linterOptions: {
+            reportUnusedDisableDirectives: 'error',
+        },
     },
 );
